870c4a7288ff757eed1ac187dd51ae9d2461f472
Alehay <alehaymkm@gmail.com>
2021-05-31 00:38:05 +0300

    sort

--- sorting/sort/merge_sort_kormen.cpp
+++ sorting/sort/merge_sort_kormen.cpp
@@ -1,19 +1,24 @@
 #include <cstdio>
 #include <cstdlib>
+#include <climits>
 
 void merge (int * arr, int iter_start_first, int iter_start_second, int lenght) {
   int left_range = iter_start_second - iter_start_first; 
   int right_range = lenght - iter_start_second;
-  int * left_arr = (int*)malloc(sizeof(int)*left_range);
-  int * right_arr = (int*)malloc(sizeof(int)*right_range);
+  int * left_arr = (int*)malloc(sizeof(int)*(left_range + 1));
+  int * right_arr = (int*)malloc(sizeof(int)*(right_range + 1));
 
   for (int i = 0; i < left_range ; ++i) {
     left_arr[i] = arr[iter_start_first + i]; 
-  }  
+  } 
+  left_arr[left_range] = INT_MAX;
+
   for (int j = 0; j < right_range; ++j) {
     right_arr[j] = arr[iter_start_second + j];
   }
-  for ( int i = 0, j = 0, k = iter_start_first ; k < lenght; ++k) {
+  right_arr[right_range] = INT_MAX;
+
+  for ( int i = 0, j = 0, k = iter_start_first ; k < iter_start_second; ++k) {
     if (left_arr[i] < right_arr[j]) {
         arr[k] = left_arr[i];
         ++i;
@@ -47,8 +52,8 @@ int main (int argc, char * argv []) {
     printf(" %d", arr[i]);
   }
   printf("\n sorted array : \n");
-  //sort(arr, lenght);
-  merge (arr, 0,3,6);
+  sort(arr, lenght);
+  //merge (arr, 0,3,6);
   
   for (int i = 0; i < lenght; ++i){
     printf(" %d",arr[i]);
