7e7ae18418884c94f5119d9bb03a328115e378e6
Alehay <alehaymkm@gmail.com>
2021-05-31 00:57:54 +0300

    merge sort

--- sorting/sort/merge_sort_kormen.cpp
+++ sorting/sort/merge_sort_kormen.cpp
@@ -2,23 +2,23 @@
 #include <cstdlib>
 #include <climits>
 
-void merge (int * arr, int iter_start_first, int iter_start_second, int lenght) {
-  int left_range = iter_start_second - iter_start_first; 
-  int right_range = lenght - iter_start_second;
+void merge (int * arr, int begin, int median, int end) {
+  int left_range = median - begin; 
+  int right_range = end - median;
   int * left_arr = (int*)malloc(sizeof(int)*(left_range + 1));
   int * right_arr = (int*)malloc(sizeof(int)*(right_range + 1));
 
   for (int i = 0; i < left_range ; ++i) {
-    left_arr[i] = arr[iter_start_first + i]; 
+    left_arr[i] = arr[begin + i]; 
   } 
   left_arr[left_range] = INT_MAX;
 
   for (int j = 0; j < right_range; ++j) {
-    right_arr[j] = arr[iter_start_second + j];
+    right_arr[j] = arr[median + j];
   }
   right_arr[right_range] = INT_MAX;
 
-  for ( int i = 0, j = 0, k = iter_start_first ; k < iter_start_second; ++k) {
+  for ( int i = 0, j = 0, k = begin ; k < end; ++k) {
     if (left_arr[i] < right_arr[j]) {
         arr[k] = left_arr[i];
         ++i;
@@ -33,12 +33,12 @@ void merge (int * arr, int iter_start_first, int iter_start_second, int lenght) 
   return;
 }
 
-void sort (int * arr, int lenght, int start = 0) {
-  if (start < lenght) {
-      int iter = (start+lenght) /2;
-      sort(arr,iter,start);
-      sort(arr,lenght,start+1);
-      merge(arr,start, iter, lenght);
+void sort (int * arr, int begin,int end ) {
+  if (begin < end) {
+      int median = (begin+end) /2;
+      sort(arr,begin,median);
+      sort(arr,median+1,end);
+      merge(arr,begin, median, end);
       return;
   }
 }
@@ -52,8 +52,8 @@ int main (int argc, char * argv []) {
     printf(" %d", arr[i]);
   }
   printf("\n sorted array : \n");
-  sort(arr, lenght);
-  //merge (arr, 0,3,6);
+  sort(arr, 0, lenght);
+  //merge (arr, 0,lenght/2,lenght);
   
   for (int i = 0; i < lenght; ++i){
     printf(" %d",arr[i]);
